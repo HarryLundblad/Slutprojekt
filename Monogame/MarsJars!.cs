4101e2a4e78e60b03b7bd406d63d8740998c216f
HarryLundblad <90604842+HarryLundblad@users.noreply.github.com>
2022-04-25 10:07:40 +0200

    Slutprojektet

---
+++ Monogame/MarsJars!.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace Monogame
+{
+    public class Game1 : Game
+    {
+        Vector2 pixel = new Vector2 (100, 300);
+        Texture2D img;
+        private GraphicsDeviceManager _graphics;
+        private SpriteBatch _spriteBatch;
+
+        public Game1()
+        {
+            _graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+            IsMouseVisible = true;
+        }
+
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here
+
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            _spriteBatch = new SpriteBatch(GraphicsDevice);
+            img = Content.Load<Texture2D>("pixel");
+        
+            // TODO: use this.Content to load your game content here
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            if (Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            if (Keyboard.GetState().IsKeyDown(Keys.Right))
+            {
+                pixel.X++;
+            }
+            if (Keyboard.GetState().IsKeyDown(Keys.Left))
+            {
+                pixel.X--;
+            }
+            
+
+            // TODO: Add your update logic here
+
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+            // TODO: Add your drawing code here
+            _spriteBatch.Begin ();
+            _spriteBatch.Draw(img, pixel, Color.White);
+            _spriteBatch.End();
+            base.Draw(gameTime);
+        }
+    }
+}
